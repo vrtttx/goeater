2acace5fb6571aedf593f3843e46d0ca9c8f7985
ynncstslv <ynncstslv@gmail.com>
2023-09-16 19:01:09 -0300

    feat: UpdateOrder controller function

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -10,7 +11,9 @@ import (
 	"github.com/vrtttx/goeater/database"
 	"github.com/vrtttx/goeater/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
@@ -64,6 +67,56 @@ func CreateOrder() gin.HandlerFunc {
 
 func UpdateOrder() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var table models.Table
+		var order models.Order
 
+		orderId := c.Param("order_id")
+
+		if err := c.BindJSON(&order); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
+			return
+		}
+		
+		var updateObj primitive.D
+
+		if order.Table_id != nil {
+			err := tableCollection.FindOne(ctx, bson.M{"table_id": order.Table_id}).Decode(&table)
+
+			defer cancel()
+
+			if err != nil {
+				msg := fmt.Sprintf("table was not found")
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+
+				return
+			}
+
+			updateObj = append(updateObj, bson.E{Key: "table_id", Value: order.Table_id})
+		}
+
+		order.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{Key: "updated_at", Value: order.Updated_at})
+
+		upsert := true
+		filter := bson.M{"order_id": orderId}
+
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		result, err := orderCollection.UpdateOne(ctx, filter, bson.D{{Key: "$set", Value: updateObj}}, &opt,)
+
+		if err != nil {
+			msg := fmt.Sprintf("order update failed")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+
+			return
+		}
+
+		defer cancel()
+
+		c.JSON(http.StatusOK, result)
 	}
 }
