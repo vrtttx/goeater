3cd0bd4d7bf9115f74ed0bd88233ff59c09492bf
ynncstslv <ynncstslv@gmail.com>
2023-09-16 19:21:56 -0300

    GetInvoices controller function

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,10 +1,50 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vrtttx/goeater/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type InvoiceViewFormat struct {
+	Invoice_id string
+	Payment_method string
+	Payment_status *string
+	Payment_due interface{}
+	Payment_due_date time.Time
+	Table_number interface{}
+	Order_id string
+	Order_details interface{}
+
+}
+
+var invoiceCollection *mongo.Collection = database.OpenCollection(database.Client, "invoice")
 
 func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := invoiceCollection.Find(context.TODO(), bson.M{})
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing invoices"})
+		}
+
+		var allInvoices []bson.M
+
+		if err = result.All(ctx, &allInvoices); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allInvoices[0])
 	}
 }
 
