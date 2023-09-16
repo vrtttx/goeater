a16b812ab68603ef2a1f023928271b674679a923
ynncstslv <ynncstslv@gmail.com>
2023-09-16 20:05:37 -0300

    feat: GetOrderItems controller function

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -1,13 +1,45 @@
 package controllers
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/vrtttx/goeater/database"
+	"github.com/vrtttx/goeater/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+type OrderItemPack struct {
+	Table_id *string
+	Order_items []models.OrderItem
+}
+
+var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItem")
 
 func GetOrderItems() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := orderItemCollection.Find(context.TODO(), bson.M{})
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing order items"})
+		}
+
+		var allOrderItems []bson.M
+
+		if err = result.All(ctx, &allOrderItems); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allOrderItems[0])
 	}
 }
 
