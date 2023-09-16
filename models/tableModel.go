9eb4e248a84809b1e227cf9330fcb49e9102f1c6
ynncstslv <ynncstslv@gmail.com>
2023-09-16 12:29:42 -0300

    fix: table model syntax error

--- models/tableModel.go
+++ models/tableModel.go
@@ -8,7 +8,7 @@ import (
 
 type Table struct {
 	ID primitive.ObjectID `bson:"id"`
-	Number_of_guests *int `json:"number_of_guests validade:"required"`
+	Number_of_guests *int `json:"number_of_guests" validade:"required"`
 	Table_number *int `json:"table_number" validade:"required"`
 	Created_at time.Time `json:"created_at"`
 	Updated_at time.Time `json:"updated_at"`
