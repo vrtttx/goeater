dd736ef7fbbc4b81ab183e22d2268b1b8a808832
ynncstslv <ynncstslv@gmail.com>
2023-09-16 15:36:07 -0300

    feat: GetFoods controller function

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -20,7 +22,91 @@ var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+
+		if err != nil || recordPerPage < 1{
+			recordPerPage = 10
+		}
+
+		page, err := strconv.Atoi(c.Query("page"))
+
+		if err != nil || page < 1 {
+			page = 1
+		}
+
+		startIndex := (page - 1) * recordPerPage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{{Key: "$match", Value: bson.D{{}}}}
+		groupStage := bson.D{
+			{
+				Key: "$group",
+				Value: bson.D{
+					{
+						Key: "_id",
+						Value: bson.D{
+							{
+								Key: "_id",
+								Value: "null",
+							},
+						},
+					},
+					{
+						Key: "total_count",
+						Value: bson.D{
+							{
+								Key: "$sum",
+								Value: 1,
+							},
+						},
+					},
+				},
+			},
+		}
+		projectStage := bson.D{
+			{
+				Key: "$project",
+				Value: bson.D{
+					{
+						Key: "_id",
+						Value: 0,
+					},
+					{
+						Key: "total_count",
+						Value: 1,
+					},
+					{
+						Key: "food_items",
+						Value: bson.D{
+							{
+								Key: "$slice",
+								Value: []interface{}{"$data", startIndex, recordPerPage}, 
+							},
+						},
+					},
+				},
+			},
+		}
+
+		result, err := foodCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, groupStage, projectStage,
+		})
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing food items"})
+		}
+
+		var allFoods []bson.M
+
+		if err = result.All(ctx, &allFoods); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allFoods[0])
 	}
 }
 
