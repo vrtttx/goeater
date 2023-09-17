725f9203b83f52db43dc6ebb0d91c3783c2ea218
ynncstslv <ynncstslv@gmail.com>
2023-09-17 10:40:04 -0300

    feat: GetUsers controller function

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,10 +1,89 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vrtttx/goeater/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
 func GetUsers() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+
+		if err != nil || recordPerPage < 1 {
+			recordPerPage = 10
+		}
+
+		page, err1 := strconv.Atoi(c.Query("page"))
+
+		if err1 != nil || page < 1 {
+			page = 1
+		}
+
+		startIndex := (page - 1) * recordPerPage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{
+			{
+				Key: "$match",
+				Value: bson.D{
+					{},
+				},
+			},
+		}
+		projectStage := bson.D{
+			{
+				Key: "$project",
+				Value: bson.D{
+					{
+						Key: "_id",
+						Value: 0,
+					},
+					{
+						Key: "total_count",
+						Value: 1,
+					},
+					{
+						Key: "user_items",
+						Value: bson.D{
+							{
+								Key: "$slice",
+								Value: []interface{}{"$data", startIndex, recordPerPage},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		result, err := userCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, projectStage,
+		})
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing users"})
+		}
+
+		var allUsers []bson.M
+
+		if err = result.All(ctx, &allUsers); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allUsers[0])
 	}
 }
 
