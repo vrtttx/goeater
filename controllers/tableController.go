b87b638911930e94f7c9cbad8794713a23c2dfec
ynncstslv <ynncstslv@gmail.com>
2023-09-16 21:09:03 -0300

    feat: GetTable controller function

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/vrtttx/goeater/database"
+	"github.com/vrtttx/goeater/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -38,7 +39,20 @@ func GetTables() gin.HandlerFunc {
 
 func GetTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		tableId := c.Param("table_id")
+		var table models.Table
+
+		err := tableCollection.FindOne(ctx, bson.M{"table_id": tableId}).Decode(&table)
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching table"})
+		}
+
+		c.JSON(http.StatusOK, table)
 	}
 }
 
