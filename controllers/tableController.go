fedbd775c573fe8b4b6595c00cf2cc13f31659a0
ynncstslv <ynncstslv@gmail.com>
2023-09-16 21:06:55 -0300

    feat: GetTables controller function

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,10 +1,38 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vrtttx/goeater/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table")
 
 func GetTables() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := tableCollection.Find(context.TODO(), bson.M{})
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing tables"})
+		}
+
+		var allTables []bson.M
+
+		if err = result.All(ctx, &allTables); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allTables[0])
 	}
 }
 
